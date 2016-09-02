4404d446d84a4bd18341d3b0e4e4e14298ecdbb1
ehimnay <himansu.h.nayak@ericsson.com>
2016-09-03 00:29:20 +0100

    beanio updated

--- beanio/src/main/java/BeanReaderMain.java
+++ beanio/src/main/java/BeanReaderMain.java
@@ -7,14 +7,19 @@ import java.util.Map;
  */
 public class BeanReaderMain {
 
-    public static void main(String...args) throws Exception {
+    public static void main(String... args) {
+        new BeanReaderMain().beanReader();
+    }
+
+    public void beanReader() {
         // create a BeanIO StreamFactory
         StreamFactory factory = StreamFactory.newInstance();
         // load the mapping file from the working directory
-        factory.load("C:\\Mercury\\github\\jse-examples\\beanio\\src\\main\\java\\contacts.xml");
+        factory.load(getClass().getClassLoader().getResource("model/contacts.xml").getFile());
 
         // create a BeanReader to read from "input.csv"
-        BeanReader in = factory.createReader("contacts", new File("C:\\Mercury\\github\\jse-examples\\beanio\\src\\main\\java\\input.csv"));
+        BeanReader in = factory.createReader("contacts",
+                getClass().getClassLoader().getResource("input.csv").getFile());
         // create a BeanWriter to write to "output.csv"
         BeanWriter out = factory.createWriter("contacts", new File("output.csv"));
 
@@ -25,15 +30,13 @@ public class BeanReaderMain {
 
             // process each record
             if ("header".equals(in.getRecordName())) {
-                Map<String,Object> header = (Map<String,Object>) record;
+                Map<String, Object> header = (Map<String, Object>) record;
                 System.out.println(header.get("fileDate"));
-            }
-            else if ("contact".equals(in.getRecordName())) {
+            } else if ("contact".equals(in.getRecordName())) {
                 Contact contact = (Contact) record;
                 // process the contact...
                 System.out.println(contact);
-            }
-            else if ("trailer".equals(in.getRecordName())) {
+            } else if ("trailer".equals(in.getRecordName())) {
                 Integer recordCount = (Integer) record;
                 System.out.println(recordCount + " contacts processed");
             }
