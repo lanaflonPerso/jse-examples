f922c725f9a3f6c191c8512d50fd3a378d1b8816
ehimnay <himansuxnayak@gmail.com>
2016-09-24 17:36:32 +0100

    code updated

---
+++ crackingthecoding/src/main/java/com/projecteuler/puzzle/SummationOfPrimes.java
@@ -0,0 +1,29 @@
+package com.projecteuler.puzzle;
+
+/**
+ * The sum of the primes below 10 is 2 + 3 + 5 + 7 = 17.
+ * <p>
+ * Find the sum of all the primes below two million.
+ */
+public class SummationOfPrimes {
+    public static void main(String...args) {
+        System.out.println(sumOfNthPrimeNumber(2_0_00_000));
+    }
+
+    public static long sumOfNthPrimeNumber(final int n) {
+        long sum = 0;
+        for (int i = 2; i < n; i++) {
+            int flag = 0;
+            for (int j = 2; j <= i; j++) {
+                if (i % j == 0) {
+                    flag = j;
+                    break;
+                }
+            }
+            if (i == flag) {
+                sum += i;
+            }
+        }
+        return sum;
+    }
+}
