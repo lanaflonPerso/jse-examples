55333a48524752efdcc30471eaae35d4ab152263
ehimnay <himansuxnayak@gmail.com>
2016-11-05 12:22:21 +0000

    problem

--- crackingthecoding/src/main/java/com/zalando/DecimalZip.java
+++ crackingthecoding/src/main/java/com/zalando/DecimalZip.java
@@ -1,14 +1,32 @@
-package main.java.com.zalando;
+package com.zalando;
 
 /**
- * Created by ehimnay on 04/11/2016.
- */
+ * The decimal zip of two non-negative integers A and B is an integer C whose decimal representation is created from the decimal representations of A and B
+ * as follows:
+ * the first (i.e. the most significant) digit of C is the first digit of A;
+ * the second digit of Cis the first digit of B;
+ * the third digit of C is the second digit of A;
+ * the fourth digit of C is the second digit of B;
+ * If one of the integers A and B runs out of digits, the remaining digits of the other integer are appended to the result.
+ * The decimal representation of 0 is assumed. be "0". For example, the decimal zip of 12 and 56 is 1526. The decimal zip of 56 and 12 is 5162.
+ * The decimal zip of 12345 and 678 is 16273845. The decimal zip of 123 and 67890 is 16273890. The decimal zip of 1234 and 0 is 10234.
+ * Write a function that, given two non-negative integers A and B, returns their decimal zip. The function should return , if the result exceeds 100,000,000.
+ * For example, given A =12345 and B = 678 the function should return 16273845, as explained above.
+ * Assume that:
+ * A and B are integers within the range [0...100,000,0001]
+ * A your solution, focus on correctness. The performance of your solution will not be the focus of the assessment
+*/
 public class DecimalZip {
-    public static void mina(String...args){
-
+    public static void main(String...args){
+        System.out.println(new DecimalZip().solution(12, 56));
+        System.out.println(new DecimalZip().solution(123, 67890));
+        System.out.println(new DecimalZip().solution(56, 12));
+        System.out.println(new DecimalZip().solution(0, 10234));
+        System.out.println(new DecimalZip().solution(12345, 678));
+        System.out.println(new DecimalZip().solution(0, 0));
     }
 
-    public static int solution(int A, int B) {
+    public int solution(int A, int B) {
         final String sA = Integer.toString(A), sB = Integer.toString(B);
         final int minLen;
 
@@ -23,14 +41,12 @@ public class DecimalZip {
             sb.append(sA.charAt(i));
             sb.append(sB.charAt(i));
         }
+
         sb.append(sA.substring(minLen, sA.length()));
         sb.append(sB.substring(minLen, sB.length()));
 
         final long zip = Long.parseLong(sb.toString());
 
-        if (zip > 100_000_000) {
-            return -1;
-        }
-        return (int)zip;
+        return zip > 100_000_000 ? -1 : (int)zip;
     }
 }
--- crackingthecoding/src/main/java/com/zalando/Password.java
+++ crackingthecoding/src/main/java/com/zalando/Password.java
@@ -1,34 +1,41 @@
-package main.java.com.zalando;
+package com.zalando;
 
 import java.util.Optional;
 import java.util.stream.Stream;
 
 /**
- * Created by ehimnay on 04/11/2016.
+ * You would like to set a password for an email account. However, there are two restrictions on the format of the password.
+ * It has to contain at least one uppercase character and it cannot contain any digits
+ * You are given a string S consisting of N alphanumerical characters. You would like to find the longest substring of S that is a valid password.
+ * A substring is defined as a contiguous segment of a string. For example, given "a0Ba" the sub-strings that are valid passwords are 1s.
+ * <p>
+ * Write a function that, given a non-empty string S consisting of N characters, returns the length of its longest substring that is a valid
+ * password. If there is no such substring, your function should return -1.
+ * For example, given "1a0Ba", your function should return 2, as explained above. Given "aebb", your function should return -1,
+ * since there is no substring that satisfies the restrictions on the format of a valid password.
  */
+
 public class Password {
-    public static void main(String[] args) throws Exception {
-        System.out.println(new Password().solution("a0Ba"));
-        System.out.println(new Password().solution("a0bb"));
+    public static void main(String... args) {
+        System.out.println(new Password().solution("a0Ba"));  // Ba
+        System.out.println(new Password().solution("a0bb"));  // bb
+        System.out.println(new Password().solution("a0bb1aB2xyZ2mnoP2aA")); // xyZ, mnoP, aA
     }
 
     public int solution(String S) {
         if (S.length() > 200) {
             throw new IllegalArgumentException("Length of the String is greater than 200");
         }
-        Optional<String> bestPassword = Stream.of(S.split("[0-9]"))
-                .filter(Password::containsCapital)
-                .sorted((o1, o2) -> o1.length() > o2.length() ? 1 : 0)
+
+        // split the string based on number
+        // filter the stream of substring containing capital letter
+        // order the substring (using comparator) based on the asc order of there size
+        // take the first one which will be the longest one.
+        Optional<String> validPassword = Stream.of(S.split("[0-9]"))
+                .filter(s -> s.chars().anyMatch(Character::isUpperCase))
+                .sorted((s1, s2) -> s2.length() - s1.length())
                 .findFirst();
 
-        if (bestPassword.isPresent()) {
-            return bestPassword.get().length();
-        } else {
-            return -1;
-        }
-    }
-
-    private static boolean containsCapital(final String word) {
-        return word.chars().anyMatch(Character::isUpperCase);
+        return validPassword.isPresent() ? validPassword.get().length() : -1;
     }
 }
